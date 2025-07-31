04fd9341c25d550bfc061db34c18c289728f9107
Kiran Kumar <barapatikirankumar2004@gmail.com>
2025-07-31 15:01:46 +0530

    Configure project for Render deployment

--- core/settings.py
+++ core/settings.py
@@ -23,12 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9cjcs&4axj$7$$ww(@$bt2&bdljt2v(a#i5@xu*su%g%gocj0d'
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1']
 
 
 # Application definition
